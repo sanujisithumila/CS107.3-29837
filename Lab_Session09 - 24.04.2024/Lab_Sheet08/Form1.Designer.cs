d25762ced8483c30149fc53b3600154d05f3253b
sanujisithumila <135606455+sanujisithumila@users.noreply.github.com>
2024-05-09 11:24:13 +0530

    Add files via upload
    Completed the Labsheet 08

---
+++ Lab_Session09 - 24.04.2024/Lab_Sheet08/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Lab_Sheet08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGrossSal = new System.Windows.Forms.TextBox();
+            this.txtTaxP = new System.Windows.Forms.TextBox();
+            this.lblTaxAmt = new System.Windows.Forms.Label();
+            this.lblNetSal = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter the details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gross Salary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(146, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tax Percentage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(146, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tax Amount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(146, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Net Salary";
+            // 
+            // txtGrossSal
+            // 
+            this.txtGrossSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrossSal.Location = new System.Drawing.Point(386, 126);
+            this.txtGrossSal.Name = "txtGrossSal";
+            this.txtGrossSal.Size = new System.Drawing.Size(271, 28);
+            this.txtGrossSal.TabIndex = 5;
+            // 
+            // txtTaxP
+            // 
+            this.txtTaxP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTaxP.Location = new System.Drawing.Point(386, 186);
+            this.txtTaxP.Name = "txtTaxP";
+            this.txtTaxP.Size = new System.Drawing.Size(271, 28);
+            this.txtTaxP.TabIndex = 6;
+            // 
+            // lblTaxAmt
+            // 
+            this.lblTaxAmt.AutoSize = true;
+            this.lblTaxAmt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaxAmt.Location = new System.Drawing.Point(390, 242);
+            this.lblTaxAmt.Name = "lblTaxAmt";
+            this.lblTaxAmt.Size = new System.Drawing.Size(75, 22);
+            this.lblTaxAmt.TabIndex = 7;
+            this.lblTaxAmt.Text = "2500.00";
+            // 
+            // lblNetSal
+            // 
+            this.lblNetSal.AutoSize = true;
+            this.lblNetSal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNetSal.Location = new System.Drawing.Point(390, 305);
+            this.lblNetSal.Name = "lblNetSal";
+            this.lblNetSal.Size = new System.Drawing.Size(85, 22);
+            this.lblNetSal.TabIndex = 8;
+            this.lblNetSal.Text = "17500.00";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(192, 366);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(130, 56);
+            this.btnCalc.TabIndex = 9;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(394, 366);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(124, 56);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 506);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblNetSal);
+            this.Controls.Add(this.lblTaxAmt);
+            this.Controls.Add(this.txtTaxP);
+            this.Controls.Add(this.txtGrossSal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGrossSal;
+        private System.Windows.Forms.TextBox txtTaxP;
+        private System.Windows.Forms.Label lblTaxAmt;
+        private System.Windows.Forms.Label lblNetSal;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Lab_Session09 - 24.04.2024/Lab_Sheet08/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_Sheet08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtGrossSal.Text = "";
+            txtTaxP.Text = "";
+            lblTaxAmt.Text = "";
+            lblNetSal.Text = "";
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            CalcTax();
+            CalcNetSalary();
+            MessageBox.Show("Net Salary:" + lblNetSal.Text, "Net Salary", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            /*double sal = Convert.ToDouble(txtGrossSal.Text);
+             double taxper = Convert.ToDouble(txtTaxP.Text);
+            double tax = CalcTax(sal,taxper);
+            double netSal = CalcNetSal(sal,tax);
+            lblTaxSal.Text = tax.Tostring();
+            lblNetSal.Text = netSal.Tostring();
+             */
+
+        }
+
+        public void CalcTax()
+        {
+
+            double sal = Convert.ToDouble(txtGrossSal.Text);
+            double taxper = Convert.ToDouble(txtTaxP.Text);
+            double tax = (sal * taxper) / 100;
+            lblTaxAmt.Text = Convert.ToDouble(tax).ToString();
+
+        }
+
+
+
+        public void CalcNetSalary()
+        {
+            double sal = Convert.ToDouble(txtGrossSal.Text);
+            double tax = Convert.ToDouble(lblTaxAmt.Text);
+            double netSal = sal - tax;
+            lblNetSal.Text = Convert.ToDouble(netSal).ToString();
+        }
+
+        /*private double CalcTax(double sal, double taxPer)
+         {
+            return (sal * taxper) / 100;
+         }
+
+          private void CalcNetSal(double sal, double tax)
+          {
+                return netSal = sal - tax;
+          } */
+
+    }
+}
